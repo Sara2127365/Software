efff03a8965741fa320931d9fc65e9f31928b675
mo3taseem666 <mo3data@gmail.com>
2025-05-10 08:51:38 +0300

    merge

--- app/(root)/(tabs)/_layout.jsx
+++ app/(root)/(tabs)/_layout.jsx
@@ -114,7 +114,6 @@ const _layout = () => {
             />
 
             <Tabs.Screen
-            <Tabs.Screen
                 name="Cart"
                 options={{
                     headerShown: false,
